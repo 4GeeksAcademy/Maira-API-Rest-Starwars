b5e00c1d5e45ca6a98b9975aff1ab4227921b6d2
Maira <maira212.a@gmail.com>
2025-12-30 17:19:17 +0000

    API Rest Starwars

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Characters, Planets, Starships, FavCharacters, FavPlanets, FavStarships
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,18 +28,23 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/users', methods=['GET'])
 def handle_hello():
-    users = User.query.all() #SELECT * FROM 'user';
+    users = User.query.all()  # SELECT * FROM 'user';
     # print(users) # [disns@4geek.com, dia@noche.com]
     users_serialized = []
     for user in users:
@@ -51,6 +57,13 @@ def handle_hello():
         'users': users_serialized
     }
     return jsonify(response_body), 200
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user = User.query.get(user_id)
+    print(user)
+    user1 = user.serialize()
+    return jsonify(user1), 200
 
 @app.route('/user', methods=['POST'])
 def add_user():
@@ -68,7 +81,7 @@ def add_user():
     new_user.is_active = True
     print(new_user)
     print(type(new_user))
-    
+
     db.session.add(new_user)  # esto agrega la nueva info
     db.session.commit()  # y esto es para guardar la nueva info en la db
     return jsonify({'msg': 'Nuevo usuario registrado', 'user': new_user.serialize()}), 200
@@ -79,20 +92,167 @@ def get_favorites(user_id):
     print(user)
     if user is None:
         return jsonify({'msg': f'El usuario con id {user_id} no existe.'}), 404
-    favorite_records_char = user.favorites_char # Lista de registros en la tabla FavCharacters
-    favorite_records_plan = user.favorites_plan
-    favorite_records_star = user.favorites_star
+
+    # Lista de registros en la tabla FavCharacters
+    favorite_records_char = user.favorites_char
     print(favorite_records_char)
-    #print(favorite_records_char[0].people.serialize())
-    print(favorite_records_char[0].people.serialize())
-
     favorite_char_serialized = []
-
     for record in favorite_records_char:
         new_favorite = record.people.serialize()
         favorite_char_serialized.append(new_favorite)
 
-    return jsonify({'msg': 'Todo salió bien', 'favorite_character': favorite_char_serialized}), 200
+    favorite_records_plan = user.favorites_plan
+    print(favorite_records_plan)
+    favorite_plan_serialized = []
+    for record in favorite_records_plan:
+        new_favorite = record.astros.serialize()
+        favorite_plan_serialized.append(new_favorite)
+
+    favorite_records_star = user.favorites_star
+    print(favorite_records_star)
+    favorite_star_serialized = []
+    for record in favorite_records_star:
+        new_favorite = record.naves.serialize()
+        favorite_star_serialized.append(new_favorite)
+
+    return jsonify({'msg': 'Todo salió bien', 'favorite_character': favorite_char_serialized, \
+                    'favorite_planets': favorite_plan_serialized, \
+                    'favorite_starships': favorite_star_serialized}), 200
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    characters = Characters.query.all()
+    characters_serialized = []
+    for character in characters:
+        characters_serialized.append(character.serialize())
+    response_body = {
+        'characters': characters_serialized
+    }
+    return jsonify(response_body), 200
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_one_character(character_id):
+    character = Characters.query.get(character_id)
+    print(character)
+    character1 = character.serialize()
+    return jsonify(character1), 200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    planets_serialized = []
+    for planet in planets:
+        planets_serialized.append(planet.serialize())
+    response_body = {
+        'planets': planets_serialized
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+    print(planet)
+    planet1 = planet.serialize()
+    return jsonify(planet1), 200
+
+@app.route('/starships', methods=['GET'])
+def get_starships():
+    starships = Starships.query.all()
+    starships_serialized = []
+    for starship in starships:
+        starships_serialized.append(starship.serialize())
+    response_body = {
+        'starships': starships_serialized
+    }
+    return jsonify(response_body), 200
+
+@app.route('/starship/<int:starship_id>', methods=['GET'])
+def get_one_starship(starship_id):
+    starship = Starships.query.get(starship_id)
+    print(starship)
+    starship1 = starship.serialize()
+    return jsonify(starship1), 200
+
+@app.route('/favorite/<int:user_id>/character/<int:character_id>', methods=['POST'])
+def add_favorite_character(user_id, character_id):
+    body = request.get_json(silent=True)
+    print(body)
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({'msg': 'El usuario no existe'}), 404
+    character = Characters.query.get(character_id)
+    if character is None:
+        return jsonify({'msg': 'El personaje no existe'}), 404
+    new_character_favorite = FavCharacters(user_id=user_id, character_id=character_id)
+    if new_character_favorite:
+        return jsonify({'msg': 'El personaje ya está en favoritos'}), 400
+    
+    db.session.add(new_character_favorite)
+    db.session.commit()
+    return jsonify({'msg': 'Nuevo favorito agregado'}), 200
+
+@app.route('/favorite/<int:user_id>/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(user_id, planet_id):
+    body = request.get_json(silent=True)
+    print(body)
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({'msg': 'El usuario no existe'}), 404
+    planet = Planets.query.get(planet_id)
+    if planet is None:
+        return jsonify({'msg': 'El planeta no existe'}), 404
+    new_planet_favorite = FavPlanets(user_id=user_id, planet_id=planet_id)
+    if new_planet_favorite:
+        return jsonify({'msg': 'El planeta ya está en favoritos'}), 400
+    
+    db.session.add(new_planet_favorite)
+    db.session.commit()
+    return jsonify({'msg': 'Nuevo favorito agregado'}), 200
+
+@app.route('/favorite/<int:user_id>/starship/<int:starship_id>', methods=['POST'])
+def add_favorite_starship(user_id, starship_id):
+    body = request.get_json(silent=True)
+    print(body)
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({'msg': 'El usuario no existe'}), 404
+    starship = Starships.query.get(starship_id)
+    if starship is None:
+        return jsonify({'msg': 'La nave no existe'}), 404
+    new_starship_favorite = FavStarships(user_id=user_id, starship_id=starship_id)
+    if new_starship_favorite:
+        return jsonify({'msg': 'Esta ya está en favoritos'}), 400
+    
+    db.session.add(new_starship_favorite)
+    db.session.commit()
+    return jsonify({'msg': 'Nuevo favorito agregado'}), 200
+
+@app.route('/favorite/<int:user_id>/character/<int:character_id>', methods=['DELETE'])
+def remove_favorite_character(user_id, character_id):
+    favorite = FavCharacters.query.filter_by(user_id=user_id, character_id=character_id).first()
+    if favorite is None:
+        return jsonify({'msg': 'Favorito no encontrado'}), 404    
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify({'msg': 'Favorito eliminado!'}), 200
+
+@app.route('/favorite/<int:user_id>/planet/<int:planet_id>', methods=['DELETE'])
+def remove_favorite_planet(user_id, planet_id):
+    favorite = FavPlanets.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+    if favorite is None:
+        return jsonify({'msg': 'Favorito no encontrado'}), 404    
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify({'msg': 'Favorito eliminado!'}), 200
+
+@app.route('/favorite/<int:user_id>/starship/<int:starship_id>', methods=['DELETE'])
+def remove_favorite_starship(user_id, starship_id):
+    favorite = FavStarships.query.filter_by(user_id=user_id, starship_id=starship_id).first()
+    if favorite is None:
+        return jsonify({'msg': 'Favorito no encontrado'}), 404    
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify({'msg': 'Favorito eliminado!'}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -10,9 +10,9 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-    favorites_char: Mapped[list['FavCharacters']] = relationship(back_populates='users_c')
-    favorites_plan: Mapped[list['FavPlanets']] = relationship(back_populates='users_p')
-    favorites_star: Mapped[list['FavStarships']] = relationship(back_populates='users_s')
+    favorites_char: Mapped[list['FavCharacters']] = relationship(back_populates='users')
+    favorites_plan: Mapped[list['FavPlanets']] = relationship(back_populates='users')
+    favorites_star: Mapped[list['FavStarships']] = relationship(back_populates='users')
 
     def serialize(self):
         return {
@@ -49,7 +49,7 @@ class FavCharacters(db.Model):
     __tablename__ = 'favorite_characters'
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    users_c: Mapped['User'] = relationship(back_populates='favorites_char')
+    users: Mapped['User'] = relationship(back_populates='favorites_char')
     character_id: Mapped[int] = mapped_column(ForeignKey('characters.id'))
     people: Mapped['Characters'] = relationship(back_populates='favorites_by_char')
 
@@ -79,12 +79,12 @@ class FavPlanets(db.Model):
     __tablename__ = 'favorite_planets'
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    users_p: Mapped['User'] = relationship(back_populates='favorites_plan')
+    users: Mapped['User'] = relationship(back_populates='favorites_plan')
     planet_id: Mapped[int] = mapped_column(ForeignKey('planets.id'))
     astros: Mapped['Planets'] = relationship(back_populates='favorites_by_plan')
 
     def __repr__(self):
-        return f'Al usuario {self.user_id} le gusta el personaje {self.planet_id}'
+        return f'Al usuario {self.user_id} le gusta el planeta {self.planet_id}'
 
 class Starships(db.Model):
     __tablename__ = 'starships'
@@ -109,9 +109,9 @@ class FavStarships(db.Model):
     __tablename__ = 'favorite_starships'
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    users_s: Mapped['User'] = relationship(back_populates='favorites_star')
+    users: Mapped['User'] = relationship(back_populates='favorites_star')
     starship_id: Mapped[int] = mapped_column(ForeignKey('starships.id'))
     naves: Mapped['Starships'] = relationship(back_populates='favorites_by_star')
 
     def __repr__(self):
-        return f'Al usuario {self.user_id} le gusta el personaje {self.starship_id}'
+        return f'Al usuario {self.user_id} le gusta la nave {self.starship_id}'
